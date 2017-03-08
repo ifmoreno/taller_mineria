ccfcf9f6d090afc4b8b66697b9c0a4b4a467e19f
Iván Moreno <Ivn Moreno>
2017-03-08 11:23:10 -0500

    quitar carpeta script

---
+++ Taller 2/Taller 2.R
@@ -0,0 +1,22 @@
+getwd()
+camino<-"C:\\Users\\ifmor\\Desktop\\Mineria de Datos\\Taller\\Taller 2\\Bases"
+setwd(camino)
+aut<-read.csv("auto.csv",header = TRUE,sep = ",",na.strings = "?")
+#al leer poner cómo son los missing
+
+fix(aut) #Muestra la base
+summary(aut) #Muestra est. básicos 
+pairs(aut[1:10,1:3]) #scatterplot matrix
+
+##Punto 1
+pairs(aut)
+##Punto 2
+mat_corr<-cor(aut[,1:8],use="pairwise.complete.obs")
+mat_corr
+##Punto3
+reg_mult<-lm( mpg~cylinders+displacement+horsepower+weight+acceleration+year+origin, data = aut)
+summary(reg_mult)
+#Punto4
+par(mfrow=c(2,2))
+plot(reg_mult)
+?plot
